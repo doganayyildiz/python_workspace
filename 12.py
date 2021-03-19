6b96ea71a8031a526f7843d47503f7787be04d70
doganayyildiz <doganayyildiz101@gmail.com>
2021-03-20 00:19:15 +0300

    ders

---
+++ 12.py
@@ -0,0 +1,16 @@
+#While Dongusu
+
+x = 0
+while x < 10:
+    print("{} degeri 10'dan kucuktur".format(x))
+    x +=1
+else: 
+    print("{} degeri 10'dan kucuk degil".format(x))
+    
+sayi = 6
+sonuc = 1
+while sayi > 0:
+    sonuc = sonuc * sayi
+    sayi -= 1
+
+print(sonuc)
