3deabf08cbc28b744374ea4a38dafe62b4e5de42
doganayyildiz <doganayyildiz101@gmail.com>
2021-03-19 14:07:08 +0300

    ilk ders

---
+++ 1.py
@@ -0,0 +1,11 @@
+# ---PRINT KULLANIMI---
+
+print("doganay")
+print("doganay \n yildiz")
+print("van \tsaray")
+
+print("benim adım {}".format('doganay'))
+print("benim adım {0},yasim {1}".format('doganay',29))
+print("benim adım {ad},yasim {yas}".format(ad='doganay',yas=29))
+
+#yükle
