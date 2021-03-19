e9126876d61b3cbc970ed0a86a34a60cd3c6ef9b
doganayyildiz <doganayyildiz101@gmail.com>
2021-03-19 14:10:35 +0300

    dersler

---
+++ 7.py
@@ -0,0 +1,13 @@
+#Sabit Koleksiyen (Tuple) Veri Tipi
+
+tup = ('a','b','c','d','e','a')
+print(tup)
+
+print(tup[0])
+
+#tup[0] = 1234 tupple veri tipinde değer değiştirilemez.
+
+print (tup.count('a')) # a harfi kaç kez tekrar ediyor
+
+print (tup.index('b')) # b hangi indekste tutuluyor
+
