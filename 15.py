c89a239cbc2516f3e5d38b85e369f1ba52a97e2f
doganayyildiz <doganayyildiz101@gmail.com>
2021-03-20 04:05:30 +0300

    DERS

--- 15.py
+++ 15.py
@@ -13,20 +13,56 @@ bes_bastir()
 def bes_dondur():
     return 5
 
-bes_dondur()
+print(bes_dondur())
 
 #Fonksiyonlarda Argumanlar
 def sayi_dondur(sayi):
     return sayi
-sayi_dondur(7)
+print(sayi_dondur(7))
 
 def dondur(sayi1=100):
     return sayi1
-dondur(10)
+print(dondur(10))
 
 def buyuk_sayi_dondur(a,b):
     if a>b:
         return a
     elif b>a:
         return b
-buyuk_sayi_dondur(3,7)
+print(buyuk_sayi_dondur(3,7))
+
+def metin_yazdir(a,b):
+    buyuk_sayi = buyuk_sayi_dondur(a,b)
+    sablon_metin = "{} daha buyuk sayidir.".format(buyuk_sayi)
+    print(sablon_metin)
+print(metin_yazdir(2,8))
+
+def isim_soyisim_ayir(isim_soyisim):
+    isim = isim_soyisim.split()[0]
+    soyisim = isim_soyisim.split()[1]
+    return isim,soyisim 
+a,b = isim_soyisim_ayir("Doganay YILDIZ")
+print(a)
+print(b)
+
+def isim_birlestir(isim,soyisim):
+    return " ".join(["Doganay","YILDIZ"]) #Liste elemanlarını birleştirir
+
+print(isim_birlestir("Doganay","YILDIZ")) #birden fazla isimli durumda hata verecek
+                                          #çünkü bu fonksiyonda 2 tane değişken var
+
+#---ARGS---
+def isim_birlestirme(*args): # sayısız parametre eklenirken args kullanılır
+    return " ".join(args) 
+print(isim_birlestirme("Mustafa","Kemal","ATATURK"))
+
+#---kwargs(Key_Words_Arguman)---
+def gobek_adi_yazdir(**kwargs):
+    if 'gobekadi' in kwargs:
+        print(kwargs['gobekadi'])
+    else:
+        print('Gobek Adi Yok')
+        
+print (gobek_adi_yazdir(adi="Doganay", gobekadi="robot", soyadi="YILDIZ"))
+print (gobek_adi_yazdir(adi="Nikola", soyadi="TESLA"))
+
